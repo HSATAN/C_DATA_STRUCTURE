55edd211eb188b92596b695aa471989571988ffb
edison <2499090390@qq.com>
2017-08-19 21:33:54 +0800

    添加欧拉回路算法

--- euler_graph.c
+++ euler_graph.c
@@ -9,3 +9,73 @@
 （b）除非无别的边可供行遍，否则ei+1不应该为Gi=G-{e1,e2, …, ei}中的桥（所谓桥是一条删除后使连通图不再连通的边）；
 （c）当（b）不能再进行时，算法停止。 
 */
+#define N 2 //矩阵的行 
+typedef struct DataNode
+{
+	struct DataNode *next;
+	int data;
+}DN;
+typedef struct HeadNode
+{
+	struct DataNode * first;
+	int degree;
+	int data;
+}HD;
+HD adj[N]; 
+void init_adj()
+{
+	//生成邻接表 
+	int i=0;
+	while(i<N)
+	{
+		adj[i].data=-1;
+		int data=0;
+		printf("请输入第 %d 个节点的出度: ",i);
+		scanf("%d",&data);
+		
+		if (data>0)
+		{
+			
+			adj[i].degree=data;
+			adj[i].first=NULL;
+		}
+		else
+		{
+			i++;
+			continue;
+		}
+		int j=0;
+		while(j<data)
+		{
+			//输入表数据 
+			DN *dn=(DN*)malloc(sizeof(DN*));
+			
+			int node_data=0;
+			scanf("%d",&node_data);
+			dn->data=node_data;
+			dn->next=adj[i].first;
+			adj[i].first=dn;
+			j++;
+		}
+		//scanf("%d",&data);
+	
+		i++;
+	}
+ } 
+ 
+ void print_tree()
+ {
+ 	//输出邻接表 
+ 	int i=0;
+ 	while(i<N)
+ 	{
+ 		DN *p=adj[i].first;
+ 		while(p)
+ 		{
+ 			printf(" %d",p->data);
+ 			p=p->next;
+		 }
+ 		
+ 		i++;
+	 }
+  } 
